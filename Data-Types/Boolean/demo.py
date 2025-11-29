b99ec32c392ee1986506c2c1881bcd058ce21f1a
mdjamilakhtar <mohmmadjamilakhtar@Mohmmads-MacBook-Air.local>
2025-11-29 23:38:42 +0530

    tuples and boolean added

---
+++ Data-Types/Boolean/demo.py
@@ -0,0 +1,5 @@
+is_logged_in = True
+is_admin = False
+
+age = 20
+is_adult = age >= 18   # True
---
+++ Data-Types/dictionaries/demo.py
@@ -0,0 +1,24 @@
+my_dict = {
+    "a": "Jamil",
+    "b": "Amit",
+    "c": "Jamal"
+}
+
+for key, value in my_dict.items():
+    print(f'{key} : {value}')
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
+}
+print(thisdict)
